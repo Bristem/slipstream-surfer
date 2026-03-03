aa933e89bf4416922a85dc044585e38be43c6b08
Brist <bristemlistem@hotmail.com>
2026-03-02 23:11:00 -0800

    Remove head

--- Player_Slipstream/Player_Slipstream.cs
+++ Player_Slipstream/Player_Slipstream.cs
@@ -27,7 +27,6 @@ datablock PlayerData(PlayerBoostArmor : PlayerStandardArmor)
    runForce = 3500;
    maxStepHeight = 0;
 };
-<<<<<<< HEAD
 
 // ===================================== //
 //                Globals
@@ -44,8 +43,6 @@ $Slips_DriftMaxLongDriftAngle = 140;
 $Slips_AuraSpeedTrigger = 130;
 
 
-=======
->>>>>>> b3f25493f29cd46de49eefbdd79952500c10d973
 // ===================================== //
 //        Player and DB Functions
 // ===================================== //
