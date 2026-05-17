3739a1aa0a5f5f4cbf4c1507ebeafb4e88d7d35e
Brist <bristemlistem@hotmail.com>
2026-05-16 18:23:07 -0700

    fix item pickup on props

--- Script_AnimatedProps.cs
+++ Script_AnimatedProps.cs
@@ -4,16 +4,21 @@ package AnimatedPropItems
     {
         %ret = parent::onAdd(%this, %obj);
 
-        if (%obj.getDatablock().isProp)
-        {
-            %obj.canPickup = false;
-        }
         if (%obj.getDatablock().passiveThread !$= "")
         {
             %obj.playThread(0, %obj.getDatablock().passiveThread);
         }
 
         return %ret;
+    }
+
+    function ItemData::onPickup(%this, %obj, %user, %amount)
+    {
+        if (%obj.getDataBlock().isProp)
+        {
+            return;
+        }
+        return parent::onPickup(%this, %obj, %user, %amount);
     }
 };
 activatePackage(AnimatedPropItems);
